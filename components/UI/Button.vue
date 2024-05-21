b0e48616fa7eada62cb32d6afade1ca846352ffc
goryfigment <goryfigment@gmail.com>
2024-05-20 21:35:01 -0700

    biltmorelitho change colors

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="p-4 transition-all ease duration-300 transform bg-white text-black dark:bg-[#060606] dark:text-white hover:scale-110">
+  <button class="p-4 transition-all ease duration-300 transform text-white bg-[#004CFF] dark:bg-[#060606] hover:scale-110">
     <slot />
   </button>
 </template>

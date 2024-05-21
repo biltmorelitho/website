75a7e7aeef2b4a2f17b0042d5956b5b3fb5936ac
goryfigment <goryfigment@gmail.com>
2024-05-21 16:17:14 -0700

    bug fixes

--- components/Section/Contact.vue
+++ components/Section/Contact.vue
@@ -9,12 +9,13 @@
 
   <a class="text-2xl m-8 hover:animate-disco" target="_blank" rel="noopener noreferrer" href="mailto:biltmorelitho@gmail.com"><b>biltmorelitho@gmail.com</b></a>
 
-  <div class="flex flex-col items-center my-2">
+  <div class="flex flex-col items-center my-2 max-w-3xl">
     <strong class="font text-[19px]">Contact Info</strong> 
     <ul class="flex flex-col text-center justify-center">
-      <li><Icon name="tdesign:building-2" /> 3612 North 7th Street<br/>Phoenix, Arizona 85014-5006</li>
-      <li><Icon name="carbon:phone" /> Telephone: (602) 263-5637</li>
-      <li><Icon name="cil:fax" /> Fax: (602) 263-8141</li>
+      <li class="flex justify-center items-center gap-1"><Icon name="tdesign:building-2" /> 3612 North 7th Street<br/></li>
+      <li class="flex justify-center">Phoenix, Arizona 85014-5006</li>
+      <li class="flex justify-center items-center gap-1"><Icon name="carbon:phone" /> Telephone: (602) 263-5637</li>
+      <li class="flex justify-center items-center gap-1"><Icon name="cil:fax" /> Fax: (602) 263-8141</li>
     </ul>
   </div>
 </template>
--- components/Section/Services.vue
+++ components/Section/Services.vue
@@ -3,16 +3,16 @@
     OUR <span class="text-[#004CFF] dark:text-[#00ff66]">SERVICES</span>
   </h2>
 
-  <div class="text-center text-[19px]">
-    <div><Icon name="streamline:business-card" /> Business Cards</div>
-    <div><Icon name="gala:brochure" /> Brochures</div>
-    <div><Icon name="hugeicons:news" /> Newsletters</div>
-    <div><Icon name="hugeicons:stationery" /> Stationery</div>
-    <div><Icon name="ri:booklet-line" /> Booklets</div>
-    <div><Icon name="solar:map-linear" /> Pamphlets</div>
-    <div><Icon name="carbon:printer" /> Digital & Offset Printing</div>
-    <div><Icon name="fa:wpforms" /> Continuous & NCR Forms</div>
-  </div>
+  <ul class="text-center text-[19px] max-w-3xl">
+    <li><Icon name="streamline:business-card" /> Business Cards</li>
+    <li><Icon name="gala:brochure" /> Brochures</li>
+    <li><Icon name="hugeicons:news" /> Newsletters</li>
+    <li><Icon name="hugeicons:stationery" /> Stationery</li>
+    <li><Icon name="ri:booklet-line" /> Booklets</li>
+    <li><Icon name="solar:map-linear" /> Pamphlets</li>
+    <li><Icon name="carbon:printer" /> Digital & Offset Printing</li>
+    <li><Icon name="fa:wpforms" /> Continuous & NCR Forms</li>
+  </ul>
 </template>
 
 <script setup>
